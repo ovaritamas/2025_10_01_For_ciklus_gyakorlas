2880e6851394e4cc672b1ff337a4c18da8fbda8a
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2025-10-01 08:44:52 +0200

    1.feladat_upd

---
+++ 1_osszegszamitas.py
@@ -0,0 +1,13 @@
+"""
+Kérj be egy egész számot (pl. 10; 13;  20…), és számítsd ki az 1-től a megadott számig terjedő egész számok összegét.
+"""
+
+szam = int(input("Adj egy egész számot, és és kiszámolom egytől a számok összegét: "))
+y = 0
+
+
+for i in range(1, szam+1):
+    y += i
+    #print(i)
+
+print(f"A számok öszzege:{y}")
